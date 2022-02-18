2bd73c662d34bfe15fdda8fd8744498b2fa8ed16
all1son4 <dm.strok98@gmail.com>
2022-02-19 01:51:28 +0300

    ;)

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -124,7 +124,7 @@ const login = (req, res, next) => {
 };
 
 const logout = (req, res) => {
-  res.cookie('jwt', '', { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: 'None', secure: true }).status(200).send({ message: 'Токен удален'})
+  res.cookie('jwt', 'none', { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: 'None', secure: true }).status(200).send({ message: 'Токен удален'})
 };
 
 module.exports = {
